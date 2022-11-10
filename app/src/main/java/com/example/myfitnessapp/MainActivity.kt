cf38f5f0b5f56217130726ae417713155396b083
sulaimankasas <ska@easypartz.com>
2022-11-10 11:35:53 +0100

    Added new layout and organized packages

--- app/src/main/java/com/example/myfitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/myfitnessapp/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.example.myfitnessapp.navbar.*
+import com.example.myfitnessapp.screen.shared.components.navbar.*
 import com.example.myfitnessapp.ui.theme.MyFitnessAppTheme
 
 class MainActivity : ComponentActivity() {
